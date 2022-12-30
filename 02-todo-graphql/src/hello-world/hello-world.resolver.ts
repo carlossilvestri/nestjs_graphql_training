377550c7b8eeed6e7483c946587582d4b3f7ca09
Carlos_Silvestri <carlosarmando99@hotmail.com>
2022-12-30 17:11:01 -0400

    Fin de la seccion 3

--- 02-todo-graphql/src/hello-world/hello-world.resolver.ts
+++ 02-todo-graphql/src/hello-world/hello-world.resolver.ts
@@ -10,8 +10,10 @@ export class HelloWorldResolver {
     getRandomNumber() : number {
         return Math.random() * 100
     }
-    @Query(() => Int, { name: "randomFromZeroTo", description: "From Zero to argument to"})
-    getRandomFromZeroTo(@Args("to") to: number) : number {
+    @Query(() => Int, { name: "randomFromZeroTo", description: "From Zero to argument to (default 6)"})
+    getRandomFromZeroTo(
+        // nullable: true le decimos que el parametro to es opcional.
+        @Args("to", { nullable: true, type: () => Int}) to: number = 6) : number {
         return Math.floor(Math.random() * to) + 1;
     }
 }
